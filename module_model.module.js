35b1926c066038440d859e3701b6f1bef1261ec7
ZergRael <zergrael@gmail.com>
2013-01-18 22:17:01 +0100

    Added a ctrl+enter form validator in textarea. closes #2 Added a model for new modules Added a global module which is always loaded Ctrl+enter while textarea is focused now validates (submit button click simulation) the parent form

---
+++ module_model.module.js
@@ -0,0 +1,36 @@
+// This file is part of GKSimproved.
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+module_.twits = {
+	name: "module_",
+	pages: [
+		{ path_name: ".*" }
+	],
+	loaded: false,
+	loadModule: function(mOptions) {
+		this.loaded = true;
+		var module_name = this.name;
+		var dbg = function(str) {
+			_dbg(module_name, str);
+		};
+
+		dbg("[Init] Loading module");
+		// Loading all functions used
+
+		dbg("[Init] Starting");
+		// Execute functions
+
+		dbg("[Init] Ready");
+	}
+};
