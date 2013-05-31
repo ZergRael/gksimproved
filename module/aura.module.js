ecb5deaa591e146dbb57122ef69d0a121696e98f
Ravomavain <ravomavain@gmail.com>
2013-05-31 20:43:32 +0200

    Add a A/Go column to aura table

--- module/aura.module.js
+++ module/aura.module.js
@@ -129,11 +129,59 @@ modules.aura = {
 			});
 		};
 
+		var updateTableSort = function()
+		{
+			dbg("[updateTableSort] Init");
+			insertScript("update_tablesorter", function(){
+				var table = $('#myauracalc');
+				$("thead tr th.tablesorter-header", table).each(function(i){
+					$(this).removeClass("tablesorter-header tablesorter-headerSortUp tablesorter-headerSortDown")
+					$(this).unbind('click mousedown');
+					var div = $(this).find('.tablesorter-header-inner')
+					if($(div).length){
+						$(this).html( $(div).text() );
+					}
+				});
+				$("thead tr.tablesorter-header", table).removeClass("tablesorter-header");
+				$(table).removeClass("tablesorter").unbind('update updateCell addRows sorton appendCache applyWidgetId applyWidgets');
+
+				$('#myauracalc').tablesorter({
+					widgets: ['zebra'],
+					widgetOptions : {
+						zebra : [ 'normal-row', 'alt-row' ]
+					},
+					sortList: [[5,1]],
+					headers: {
+						2: {sorter: 'filesize'},
+						3: {sorter: false},
+					}
+				});
+			}, true);
+			dbg("[updateTableSort] Done");
+		}
+
+		var addAgColumn = function()
+		{
+			dbg("[addAgColumn] Starting");
+			var myauracalc = $('#myauracalc');
+			$("thead tr th:last", myauracalc).after("<th class='ag_column'>A/Go</th>");
+			$("tbody tr", myauracalc).each(function(i){
+				var tds = $(this).find("td");
+				var size = tds.eq(2).attr("data-filesize")/(1024*1024*1024);
+				var A = tds.eq(4).text();
+				tds.last().after("<td class='ag_column'>"+(A/size).toFixed(3)+"</td>");
+			});
+			updateTableSort();
+			dbg("[addAgColumn] Done");
+		}
+
 		dbg("[Init] Starting");
 		// Execute functions
 
 		var buttons = '<table id="gksi_aura_controls" class="table100 tablesorter" style="width: 100%;" border="1"><thead><tr><th>Calculateur d\'objectif de karma</th><th>Calculateur de palier d\'aura</th><th>Calculateur de changements d\'aura</th></tr></thead><tbody><tr><td>Objectif karma : <input id="gksi_karmagoal_input" type="text" value="1000000"><br>Temps nécessaire estimé : <span id="gksi_karmagoal_time" class="gksi_aura_result"></span></td><td>Objectif aura : <input id="gksi_auragoal_input" type="text" value="49.9"><br>Taux Ag de progression : <span id="gksi_auragoal_progression">Z</span> / Go <a href="#" title="Cette valeur est calculée à partir de l\'ensemble des caractéristiques de torrents que vous seedez (nombre de seeds et age moyen).\n\nElle permet de déduire la progression de votre aura en fonction d\'une taille en Go. On trouvera généralement une valeur proche de 1. Si vos torrents sont jeunes et très seedés la valeur sera plus faible, et inversement pour des torrents agés et peu seedés.">[?]</a><br>Go nécessaires estimés : <span id="gksi_auragoal_go" class="gksi_aura_result">Z</span> Go</td><td>Lien vers un torrent à télécharger : <input id="gksi_auratorrent_input" type="text"><br>En seedant ce torrent, votre aura passera à : <span id="gksi_auratorrent_aura" class="gksi_aura_result"></span></td></tr></tbody></table><p class="marginleft">Attention, ce système expérimental ne propose que des estimations servant à vous aider dans l\'optimisation de votre aura/karma.<br />Malgré l\'utilisation des formules exactes du calcul d\'aura, ces valeurs sont des approximations basées sur les arrondis de la page.</p><br />';
 		$(mOptions.buttons).before(buttons);
+
+		addAgColumn();
 
 		$("#gksi_karmagoal_input").keyup(calcKarmaGoal);
 		$("#gksi_auragoal_input").keyup(calcAuraGoal);
