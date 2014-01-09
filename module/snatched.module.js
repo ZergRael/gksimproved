f35f7aba44ba997f7be2dc2e635d11eaa70c9977
ZergRael <zergrael@gmail.com>
2014-01-09 17:56:16 +0100

    Switched snatched sorter to $.tablesorter

--- module/snatched.module.js
+++ module/snatched.module.js
@@ -21,19 +21,42 @@ modules.snatched = {
 				var node = $(this);
 				var t = {node: node, status: {}, shown: true};
 				node.find("td").each(function(i) {
-					if(i === 0 && $(this).text() == "Torrent Supprimé") {
-						t.status.deleted = true;
+					if(i === 0) {
+						t.name = $(this).text();
+						if(t.name == "Torrent Supprimé") {
+							t.name = null;
+							t.status.deleted = true;
+						}
 					}
-					if(i == 1 && $(this).find('img').attr('src') == "https://s.gks.gs/static/themes/sifuture/img/validate.png") {
+					else if(i == 1 && $(this).find('img').attr('src') == "https://s.gks.gs/static/themes/sifuture/img/validate.png") {
 						t.status.seeding = true;
 					}
-					if(i == 5 && $(this).text() != "Non Complété") {
-						t.status.completed = true;
+					else if(i == 2) {
+						t.ul = Number($(this).attr("data-filesize"));
 					}
-					if(i == 5 && $(this).find("img").length) {
-						t.status.hnr = true;
+					else if(i == 3) {
+						t.dl = Number($(this).attr("data-filesize"));
+					}
+					else if(i == 4) {
+						t.rdl = Number($(this).attr("data-filesize"));
+					}
+					else if(i == 5) {
+						t.seedtime = dateToInt($(this).text());
+						$(this).attr("data-duration", t.seedtime);
+						if($(this).text() != "Non Complété") {
+							t.status.completed = true;
+						}
+						if($(this).find("img").length) {
+							t.status.hnr = true;
+						}
+					}
+					else if(i == 6) {
+						var text = $(this).text();
+						t.ratio = (text == "--" ? 0 : (text == "∞" ? 999999999 : Number(text.replace(',', ''))));
+						$(this).attr("data-ratio", t.ratio);
 					}
 				});
+				node.data("t", t);
 				torrentList.push(t);
 			});
 			dbg("[TorrentTag] Ended scanning");
@@ -92,25 +115,6 @@ modules.snatched = {
 			}
 		};
 
-		var bytesToInt = function(str) {
-			var splitted = str.split(" ");
-			if(splitted.length != 2) {
-				return str;
-			}
-
-			var oInt = Number(splitted[0]);
-			if(splitted[1] == "Mo") {
-				return oInt * 1024;
-			}
-			if(splitted[1] == "Go") {
-				return oInt * 1048576;
-			}
-			if(splitted[1] == "To") {
-				return oInt * 1073741824;
-			}
-			return oInt;
-		};
-
 		var dateToInt = function(str) {
 			if(str == "Non Complété") {
 				return 0;
@@ -119,60 +123,54 @@ modules.snatched = {
 			return ((regStr[1] ? Number(regStr[1]) : 0) * 31104000 + (regStr[2] ? Number(regStr[2]) : 0) * 2592000 + (regStr[3] ? Number(regStr[3]) : 0) * 604800 + (regStr[4] ? Number(regStr[4]) : 0) * 86400 + (regStr[5] ? Number(regStr[5]) : 0) * 3600 + (regStr[6] ? Number(regStr[6]) : 0) * 60 + (regStr[7] ? Number(regStr[7]) : 0));
 		};
 
-		var sort = false;
-		var sortData = function() {
-			if(!sort) {
-				return;
-			}
+		var addSorter = function() {
+			insertScript("native_tablesorter", function() {
+				$.tablesorter.addParser({
+					id: 'duration',
+					is: function(s) {
+						return false;
+					},
+					format: function(s, table, cell, cellIndex) {
+						var data, $cell = $(cell);
+						return $cell.attr('data-duration') || s;
+					},
+					type: 'numeric'
+				});
+				$.tablesorter.addParser({
+					id: 'ratio',
+					is: function(s) {
+						return false;
+					},
+					format: function(s, table, cell, cellIndex) {
+						var data, $cell = $(cell);
+						return $cell.attr('data-ratio') || s;
+					},
+					type: 'numeric'
+				});
 
-			var sortFunc = false;
-			switch(sort) {
-				case "sortName":
-					sortFunc = function(a, b) {
-						var aN = $(a).find("td:nth-child(1) a").text();
-						var bN = $(b).find("td:nth-child(1) a").text();
-						return order == "desc" ? (aN > bN ? -1 : 1) : (aN > bN ? 1 : -1);
-					};
-					break;
-				case "sortUL":
-					sortFunc = function(a, b) {
-						var aN = $(a).find("td:nth-child(3)").text();
-						var bN = $(b).find("td:nth-child(3)").text();
-						return order == "desc" ? (bytesToInt(aN) > bytesToInt(bN) ? -1 : 1) : (bytesToInt(aN) > bytesToInt(bN) ? 1 : -1);
-					};
-					break;
-				case "sortDL":
-					sortFunc = function(a, b) {
-						var aN = $(a).find("td:nth-child(4)").text();
-						var bN = $(b).find("td:nth-child(4)").text();
-						return order == "desc" ? (bytesToInt(aN) > bytesToInt(bN) ? -1 : 1) : (bytesToInt(aN) > bytesToInt(bN) ? 1 : -1);
-					};
-					break;
-				case "sortRDL":
-					sortFunc = function(a, b) {
-						var aN = $(a).find("td:nth-child(5)").text();
-						var bN = $(b).find("td:nth-child(5)").text();
-						return order == "desc" ? (bytesToInt(aN) > bytesToInt(bN) ? -1 : 1) : (bytesToInt(aN) > bytesToInt(bN) ? 1 : -1);
-					};
-					break;
-				case "sortST":
-					sortFunc = function(a, b) {
-						var aN = $(a).find("td:nth-child(6)").text();
-						var bN = $(b).find("td:nth-child(6)").text();
-						return order == "desc" ? (dateToInt(aN) > dateToInt(bN) ? -1 : 1) : (dateToInt(aN) > dateToInt(bN) ? 1 : -1);
-					};
-					break;
-				case "sortRatio":
-					sortFunc = function(a, b) {
-						var aN = $(a).find("td:nth-child(7)").text();
-						var bN = $(b).find("td:nth-child(7)").text();
-						aN = (aN == "--" ? 0 : (aN == "∞" ? 999999999 : Number(aN)));
-						bN = (bN == "--" ? 0 : (bN == "∞" ? 999999999 : Number(bN)));
-						return order == "desc" ? (aN > bN ? -1 : 1) : (aN > bN ? 1 : -1);
-					};
-					break;
-			}
-			$(".table100 tbody").html($(".table100 tbody tr").sort(sortFunc));
+				$('.tablesorter').tablesorter({
+					widgets: ['zebra'],
+					widgetOptions : {
+						zebra : [ 'normal-row', 'alt-row' ]
+					},
+					headers: {
+						//0 // Name
+						1: {sorter: false}, // Seeding
+						2: {sorter: 'filesize'}, // UL
+						3: {sorter: 'filesize'}, // DL
+						4: {sorter: 'filesize'}, // RDL
+						5: {sorter: 'duration'}, // Seedtime
+						6: {sorter: 'ratio'},
+						7: {sorter: false} // DL button
+					}
+				});
+			});
+		};
+
+		var updateSorter = function() {
+			insertScript("table_sorting", function() {
+				$(".tablesorter").trigger("update");
+			}, true);
 		};
 
 		var maxPage = modules["endless_scrolling"].maxPage, thisPage = modules["endless_scrolling"].thisPage;
@@ -210,8 +208,8 @@ modules.snatched = {
 					if(pageLoaded == maxPage) {
 						$(".page_loading").remove();
 						dbg("[AllPagesGrab] Grabbing ended");
+						updateSorter();
 						applyFilters();
-						sortData();
 						$(document).trigger("es_dom_process_done");
 					}
 				});
@@ -225,26 +223,8 @@ modules.snatched = {
 		dbg("[Init] Starting");
 		// Adding buttons
 		var torrentButtons = ' | <span class="g_filter g_filter_' + opt.get(module_name, "filter_deleted") + '" opt="filter_deleted">Supprimés</span> | <span class="g_filter g_filter_' + opt.get(module_name, "filter_seed") + '" opt="filter_seed">En seed</span> | <span class="g_filter g_filter_' + opt.get(module_name, "filter_complete") + '" opt="filter_complete">Completés</span> | <span class="g_filter g_filter_' + opt.get(module_name, "filter_hnr") + '" opt="filter_hnr">Hit&Run</span>' + (canGrabAllPages ? '<span id="grabAllPagesSpan"> | <a href="#" id="grabAllPages">Récupérer toutes les pages</a></span>' : '');
-		var colSortButtons = [ {n: 1, id: "sortName", nom: "Nom"}, {n: 3, id: "sortUL", nom: "UL"}, {n: 4, id: "sortDL", nom: "DL"}, {n: 5, id: "sortRDL", nom: "Real DL"}, {n: 6, id: "sortST", nom: "SeedTime"}, {n: 7, id: "sortRatio", nom: "Ratio"}
-		];
 
 		$(mOptions.buttons).after(torrentButtons);
-		$.each(colSortButtons, function(k, v) {
-			$(".table100 thead tr th:nth-child(" + v.n + ")").wrapInner('<a id="' + v.id + '" class="sortCol" href="#">');
-		});
-
-		// Sort on column click
-		$(".sortCol").click(function() {
-			if(sort == $(this).attr("id")) {
-				order = (order == "desc" ? "asc" : "desc");
-			}
-			else {
-				sort = $(this).attr("id");
-				order = "asc";
-			}
-			sortData();
-			return false;
-		});
 
 		// No reason to show grabber if not on first page
 		if(canGrabAllPages) {
@@ -265,12 +245,13 @@ modules.snatched = {
 		});
 
 		tagTorrents($(".table100 tbody tr"));
+		addSorter();
 		filtersChanged();
 
 		$(document).on("endless_scrolling_insertion_done", function() {
 			dbg("[endless_scrolling] Module specific functions");
+			updateSorter();
 			applyFilters();
-			sortData();
 			canGrabAllPages = false;
 			$("#grabAllPagesSpan").remove();
 			$(document).trigger("es_dom_process_done");
